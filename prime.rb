034c02af6fd9f6715769b0b80b5b076ec48c77e3
han xu <hanxu27@gmail.com>
2019-04-07 00:42:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,16 @@ def prime?(number)
   array = []
   i = 2
   prime = true
+  counter = 0
   
   while i < number/2
     array << i
   end
   
   array.each do |div|
-    
+    if number % array[counter] == 0
+      prime = false
+      break
+    end
+    counter += 1
+  
